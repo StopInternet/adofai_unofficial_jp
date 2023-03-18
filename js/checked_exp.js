cb23910c7137589b20a5c8eaadc0dda2f98f9241
Stop__Internets <69037662+StopInternet@users.noreply.github.com>
2023-03-18 12:25:58 +0900

    経験値シミュレーターを実装

--- js/checked_exp.js
+++ js/checked_exp.js
@@ -12,3 +12,69 @@ function calc(){
     }
     document.getElementById("ans").innerHTML = "<h5>獲得経験値(計算結果)："+Math.round(calc)+"</h5>"
 }
+
+function calc_dojo(){
+    //ID読み込み
+    var d_level = document.getElementById("level_exp_dojo").value
+    var d_speed_1 = document.getElementById("speed_exp1").value
+    var d_speed_2 = document.getElementById("speed_exp2").value
+    var d_speed_3 = document.getElementById("speed_exp3").value
+    var d_speed_4 = document.getElementById("speed_exp4").value
+    var d_speed_5 = document.getElementById("speed_exp5").value
+    var d_speed_6 = document.getElementById("speed_exp6").value
+
+    //経験値挿入
+    if(d_level == 1){
+        d_level = 10000;
+    }
+    if(d_level == 2){
+        d_level = 20000;
+    }
+    if(d_level == 3){
+        d_level = 30000;
+    }
+    if(d_level == 4){
+        d_level = 40000;
+    }
+    if(d_level == 5){
+        d_level = 50000;
+    }
+    if(d_level == 6){
+        d_level = 60000;
+    }
+    var calc_d = 0;
+
+    //計算式
+    if(d_speed_1 == 1){
+        calc_d = d_level * (1+(d_speed_2*d_speed_3*d_speed_4*d_speed_5*d_speed_6)/100)
+    }
+    if(d_speed_2 == 1){
+        calc_d = d_level * (1+(d_speed_1*d_speed_3*d_speed_4*d_speed_5*d_speed_6)/100)
+    }
+    if(d_speed_3 == 1){
+        calc_d = d_level * (1+(d_speed_1*d_speed_2*d_speed_4*d_speed_5*d_speed_6)/100)
+    }
+    if(d_speed_4 == 1){
+        calc_d = d_level * (1+(d_speed_1*d_speed_2*d_speed_3*d_speed_5*d_speed_6)/100)
+    }
+    if(d_speed_5 == 1){
+        calc_d = d_level * (1+(d_speed_1*d_speed_2*d_speed_3*d_speed_4*d_speed_6)/100)
+    }
+    if(d_speed_6 == 1){
+        calc_d = d_level * (1+(d_speed_1*d_speed_2*d_speed_3*d_speed_4*d_speed_5)/100)
+    }
+    if(d_speed_1 == 1 &&
+             d_speed_2 == 1 &&
+             d_speed_3 == 1 &&
+             d_speed_4 == 1 &&
+             d_speed_5 == 1 &&
+             d_speed_6 == 1){
+                calc_d = d_level
+    }
+    else{
+        calc_d = d_level * (1+(d_speed_1*d_speed_2*d_speed_3*d_speed_4*d_speed_5*d_speed_6)/100)
+    }
+    
+    //結果挿入
+    document.getElementById("ans2").innerHTML = "<h5>獲得経験値(計算結果)："+Math.round(calc_d)+"</h5>"
+}
---
+++ js/dojo.js
@@ -0,0 +1,139 @@
+//✪１ 単純に難易度が低い
+star1_level = {
+    1:3,
+    2:4,
+    3:5,
+    4:5,
+    5:6,
+    6:7
+}
+
+star1_steamurl = {
+    1:'https://steamcommunity.com/sharedfiles/filedetails/?id=2312701331',
+    2:'https://steamcommunity.com/sharedfiles/filedetails/?id=2605834847',
+    3:'https://steamcommunity.com/sharedfiles/filedetails/?id=2853267842',
+    4:'',
+    5:'',
+    6:'',
+}
+
+star1_yturl = {
+    1:'<iframe width="560" height="315" src="https://www.youtube.com/embed/BOWjYTEOnnY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+    2:'<iframe width="560" height="315" src="https://www.youtube.com/embed/1VwX_UXgCeI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+    3:'<iframe width="560" height="315" src="https://www.youtube.com/embed/IrT9qbjOiUY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+    4:'',
+    5:'',
+    6:'',
+}
+
+//✪2　✪１よりプラスアップ
+star2_level = {
+    1:3,
+    2:4,
+    3:5,
+    4:5,
+    5:6,
+    6:7
+}
+
+star2_steamurl = {
+    1:'',
+    2:'',
+    3:'',
+    4:'',
+    5:'',
+    6:'',
+}
+
+star2_yturl = {
+    1:'',
+    2:'',
+    3:'',
+    4:'',
+    5:'',
+    6:'',
+}
+
+//✪3　難易度は✪１同等だが最大タイル数は多め
+star3_level = {
+    1:3,
+    2:4,
+    3:5,
+    4:5,
+    5:6,
+    6:7
+}
+
+star3_steamurl = {
+    1:'',
+    2:'',
+    3:'',
+    4:'',
+    5:'',
+    6:'',
+}
+
+star3_yturl = {
+    1:'',
+    2:'',
+    3:'',
+    4:'',
+    5:'',
+    6:'',
+}
+
+//✪4　難易度は✪２同等だが最大BPMは高い
+star4_level = {
+    1:3,
+    2:4,
+    3:5,
+    4:5,
+    5:6,
+    6:7
+}
+
+star4_steamurl = {
+    1:'',
+    2:'',
+    3:'',
+    4:'',
+    5:'',
+    6:'',
+}
+
+star4_yturl = {
+    1:'',
+    2:'',
+    3:'',
+    4:'',
+    5:'',
+    6:'',
+}
+
+//✪5　単純に難易度が高め
+star5_level = {
+    1:3,
+    2:4,
+    3:5,
+    4:5,
+    5:6,
+    6:7
+}
+
+star5_steamurl = {
+    1:'',
+    2:'',
+    3:'',
+    4:'',
+    5:'',
+    6:'',
+}
+
+star5_yturl = {
+    1:'',
+    2:'',
+    3:'',
+    4:'',
+    5:'',
+    6:'',
+}
